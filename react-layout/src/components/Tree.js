88c1f2ca9133347a6dca6cbb71c0529cf036649e
GMGustafson <gustafsg@email.sc.edu>
2024-10-29 12:41:04 -0400

    everything we did in class on 10.29

---
+++ react-layout/src/components/Tree.js
@@ -0,0 +1,24 @@
+import "../css/Tree.css"; 
+import {useState} from "react"; 
+
+const Tree = (tree) => 
+{ 
+    const[pruned, pruneTree] = useState(false); 
+
+    const doTreeJob = () => 
+    { 
+        pruneTree(true); 
+    }
+
+
+    return ( 
+        <section className= "tree">
+            <h3 className={pruned?"pruned":""}> {tree.name}</h3>
+            <img src={tree.image} alt={tree.name}/>
+            <button onClick ={doTreeJob}> Purne Tree </button>
+        </section>
+    )
+}; 
+
+
+export default Tree; 
--- react-layout/src/pages/Home.js
+++ react-layout/src/pages/Home.js
@@ -1,5 +1,15 @@
+
+import Tree from "../components/Tree.js"; 
+import "../css/Home.css"; 
+
 const Home = () => {
-    return <h1> Home Page</h1>;
+    return (
+        <div className = "trees"> 
+            <Tree name="bald cypress" image = "images/bald.jpg" /> 
+            <Tree name = "eastern redbud" image = "images/red-bud.jpg" /> 
+            <Tree name = "live oak" image = "images/live-oak.jpg" /> 
+        </div>
+    )
 };
 
 export default Home; 
