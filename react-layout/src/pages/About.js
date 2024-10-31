f0a3307753129426507ba269f34f241c4bbc1809
GMGustafson <gustafsg@email.sc.edu>
2024-10-31 17:32:38 -0400

    10.31

--- react-layout/src/pages/About.js
+++ react-layout/src/pages/About.js
@@ -1,14 +1,15 @@
-import SlideShow from "../components/Slideshow";
+import SlideShow from "../components/SlideShow";
+import HousePlans from "../components/HousePlans";
 
 const About = () => {
-    return 
-    (
-        <> 
+    return (
+        <>
+            <h3>About</h3>
+            <SlideShow />
 
-        
+            <HousePlans />
         </>
-    )
-    
+    );
 };
-    
-export default About; 
+
+export default About;
