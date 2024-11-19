de23962c3fc77d3d3f4350eae00ce7ddfa746354
GMGustafson <gustafsg@email.sc.edu>
2024-11-19 12:11:56 -0500

    changes

--- react-layout/src/components/DeleteHousePlan.js
+++ react-layout/src/components/DeleteHousePlan.js
@@ -4,6 +4,11 @@ import React, { useState } from "react";
 const DeleteHousePlan = (props) => {
   const [result, setResult] = useState("");
 
+  const deleteHousePlan = async() => {
+    const response = await fetch(`https://localhost:3001/api/house_plans/${props.id}`, 
+      {method:"DELETE"}); 
+  };
+
   return (
     <div id="delete-dialog" className="w3-modal">
       <div className="w3-modal-content">
@@ -19,6 +24,7 @@ const DeleteHousePlan = (props) => {
             <h3>Are you sure you want to delete the {props.name}</h3>
             <section>
               <button onClick={props.closeDialog}>No</button>
+              <button onClick={deleteHousePlan}>Yes</button>
             </section>
             <span>{result}</span>
           </div>
